ab787fe906a59bdc3c344bdd48fdacc2137ce7d6
Roman Oryol <roman.oryol83@gmail.com>
2025-03-24 11:23:39 +0200

    feat: добавить стили для заголовков и улучшить создание заголовков

--- src/js/components/title.js
+++ src/js/components/title.js
@@ -1,6 +1,9 @@
+import '../../styles/components/titles.css';
+
 export default function createTitle(level, textSizeClass, text) {
   const title = document.createElement(level);
   title.textContent = text;
   title.classList.add(textSizeClass);
+  title.classList.add(level === 'h1' ? 'title' : 'subtitle');
   return title;
 }
